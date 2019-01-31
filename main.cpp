324a90616d8705d364a100d25e5d600c688d4a4f
scottey24 <46961419+scottey24@users.noreply.github.com>
2019-01-30 19:24:20 -0600

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Week 2 Lab";
+	int bob;
+	int john;
+	int jimbob = bob+ john;
+	
+	bob = 42;
+	john = 32;
+	int jimbob = bob+ john;
+	short rachel = 10;
+	short luke = 7;
+	float pie = 3.14;
+	char middle = 'M';
+	
+	
+	cout << "Bob and John together are";
+	cout << jimbob << " years old" << endl;
+	count << middle << " is the middle initial for ";
+	cout << pie << " users that are" << luke ;
+	cout << " year old.";
+	
+	return0;
+}
