f3a88f3b35bd3b8c761ac78c0cc906687b114dca
scottey24 <46961419+scottey24@users.noreply.github.com>
2019-01-30 19:47:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,32 @@ include <iostream>
 using namespace std;
 
 int main(){
-	cout << "Week 2 Lab";
-	int bob;
-	int john;
-	int jimbob = bob+ john;
+	cout << "Week 2 Lab:" ,, endl;
 	
-	bob = 42;
-	john = 32;
+	int bob = 42;
+	int john = 32;
 	int jimbob = bob+ john;
 	short rachel = 10;
 	short luke = 7;
 	float pie = 3.14;
 	char middle = 'M';
+	bool shouldWe = false;
+	
+	if( jimbob == luke) {
+		cout << " the result was true" << endl;
+	} else {
+		cout << " FALSE!!!!" << endl;
+	}
+		
 	
 	
+
 	cout << "Bob and John together are";
-	cout << jimbob << " years old" << endl;
-	count << middle << " is the middle initial for ";
-	cout << pie << " users that are" << luke ;
-	cout << " year old.";
+	cout << jimbob << " is bob and jim added" << endl;
+	count << jbob - john << " is bob and jim subtracted " << endl;
+	cout << bob/john << " is bob and jim divided " << endl;
+	cout << bob * john << " is bob and jim multiplied" endl;
+	cout << bob%john << : is bob modulas jim" << endl;
 	
 	return0;
 }
