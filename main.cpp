98098a37448f084711d265e3852a2853eff8ed81
scottey24 <46961419+scottey24@users.noreply.github.com>
2019-01-30 20:11:18 -0600

    counting

--- main.cpp
+++ main.cpp
@@ -2,18 +2,15 @@ include <iostream>
 using namespace std;
 
 int main(){
-	char game = 'Y';
 	
-	while(('Y' == game) || ('y' == game)){
-		cout << " Do you want to play a game?" << endl;
-		cout << " (Y/n)" << endl;
-		cin >> game;
+	int number =1;
+	int total = 0;
 	
-		if( ('Y' == game) || ('y' == game)){
-		cout <<" How about thermonuclear war?" <<endl;
-		} else {
-		cout << " Goodbye then " << endl;
-		}
+	while(number <= 1000) {
+		cout << " Our number is " << number << endl;
+		total = total = number;
+		cout << " Our total is " << total<< endl;
+		number++;
 	}
 	
 	
